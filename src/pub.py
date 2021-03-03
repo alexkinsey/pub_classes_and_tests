f791c4d53cff51d911ca7c77a3b5bac70b4080c1
alexkinsey <alexanderkinsey@icloud.com>
2021-03-03 19:44:41 +0000

    condenced pub class to make code more efficent

--- src/pub.py
+++ src/pub.py
@@ -7,44 +7,32 @@ class Pub:
             "food": food
             } 
 
+    # add or remove cash from till using positive or negative amount
     def till_transaction(self, amount):
         self.till += amount
 
-    def find_drink_by_name(self, name):
-        for drink in self.stock["drinks"]:
-            if drink.name == name:
-                return drink
-        return None
-        
-    def find_food_by_name(self, name):
-        for food in self.stock["food"]:
-            if food.name == name:
-                return food
+    # find any item, must pass key for item_type (food or drinks) as a string
+    def find_item_by_name(self, item_type, item_name):
+        for item in self.stock[item_type]:
+            if item.name == item_name:
+                return item
         return None
 
-    def sell_drink(self, customer, drink_name):
-        drink = self.find_drink_by_name(drink_name)
-        if customer.age < 18 or customer.drunk_level > 10 or not drink or customer.wallet < drink.price:
-            return "could not sell drink"
-        if self.check_stock_amount("drinks", drink.name) < 1:
+    # sell any stocked item if customer meets conditions
+    def sell_item(self, customer, item_type, item_name, amount = 1):
+        item = self.find_item_by_name(item_type, item_name)
+
+        if customer.age < 18 or customer.drunk_level > 10 or not item or customer.wallet < item.price * amount:
+            return "could not sell item"
+        if self.check_stock_amount(item_type, item.name) < amount:
             return "no stock"
 
-        customer.reduce_wallet(drink.price)
-        customer.increase_or_decrease_drunk_level(drink.alcohol_level)
-        self.till_transaction(drink.price)
-        self.change_stock_amount("drinks", drink.name, -1)
+        customer.reduce_wallet(item.price * amount)
+        customer.increase_or_decrease_drunk_level(item.alcohol_level * amount)
+        self.till_transaction(item.price * amount)
+        self.change_stock_amount(item_type, item.name, - amount)
 
-
-    def sell_food(self, customer, food_name):
-        food = self.find_food_by_name(food_name)
-        if not food or customer.wallet < food.price:
-            return "could not sell food"
-
-        customer.reduce_wallet(food.price)
-        customer.increase_or_decrease_drunk_level(food.alcohol_level)
-        self.till_transaction(food.price)
-        self.change_stock_amount("food", food.name, -1)
-
+    # check total value of stock and return value
     def check_stock_total_value(self):
         total = 0
         
@@ -55,13 +43,15 @@ class Pub:
 
         return total
 
-    def change_stock_amount(self, key, item, amount):
-        for i in range(len(self.stock[key])):
-            if self.stock[key][i].name == item:
-                 self.stock[key][i].stock += amount
+    # change the stock amount by passing positive or negative amount
+    def change_stock_amount(self, item_type, item_name, amount):
+        for i in range(len(self.stock[item_type])):
+            if self.stock[item_type][i].name == item_name:
+                 self.stock[item_type][i].stock += amount
                  
-    def check_stock_amount(self, key, item):
-        for i in range(len(self.stock[key])):
-            if self.stock[key][i].name == item:
-                return self.stock[key][i].stock
+    # return stock value of an item
+    def check_stock_amount(self, item_type, item_name):
+        for i in range(len(self.stock[item_type])):
+            if self.stock[item_type][i].name == item_name:
+                return self.stock[item_type][i].stock
     
--- tests/pub_test.py
+++ tests/pub_test.py
@@ -28,41 +28,41 @@ class TestPub(unittest.TestCase):
         self.assertEqual(1600, self.pub.till)
 
     def test_get_drink_name(self):
-        find_drink = self.pub.find_drink_by_name("Long Vodka")
+        find_drink = self.pub.find_item_by_name("drinks", "Long Vodka")
         self.assertEqual("Long Vodka", find_drink.name)
 
     def test_sell_drink_to_customer(self):
         customer = Customer("Bob", 100.00, 25)
-        self.pub.sell_drink(customer, "Bramble")
+        self.pub.sell_item(customer, "drinks", "Bramble")
         self.assertEqual(1505.00, self.pub.till)
         self.assertEqual(95.00, customer.wallet)
         self.assertEqual(19, self.pub.stock["drinks"][0].stock)
 
     def test_sell_drink_to_customer_drink__not_exist(self):
         customer = Customer("Bob", 100.00, 25)
-        self.assertEqual("could not sell drink", self.pub.sell_drink(customer, "Beer"))
+        self.assertEqual("could not sell item", self.pub.sell_item(customer, "drinks", "Beer"))
 
     def test_sell_drink_to_customer_drink_found_no_funds(self):
         customer = Customer("Bob", 3.00, 25)
-        self.assertEqual("could not sell drink", self.pub.sell_drink(customer, "Bramble"))
+        self.assertEqual("could not sell item", self.pub.sell_item(customer, "drinks", "Bramble"))
 
     def test_sell_drink_to_customer_underage(self):
         customer = Customer("Bob", 100.00, 8)
-        self.assertEqual("could not sell drink", self.pub.sell_drink(customer, "Bramble"))
+        self.assertEqual("could not sell item", self.pub.sell_item(customer, "drinks", "Bramble"))
     
     def test_sell_drink_to_customer_drunk(self):
         customer = Customer("Bob", 100.00, 25, 9)
-        self.pub.sell_drink(customer, "Bramble")
-        self.assertEqual("could not sell drink", self.pub.sell_drink(customer, "Bramble"))
+        self.pub.sell_item(customer, "drinks", "Bramble")
+        self.assertEqual("could not sell item", self.pub.sell_item(customer, "drinks", "Bramble"))
 
     def test_sell_food_to_customer_decrease_drunk_level(self):
         customer = Customer("Bob", 100.00, 25, 10)
-        self.pub.sell_food(customer, "Pizza")
+        self.pub.sell_item(customer, "food", "Pizza")
         self.assertEqual(4, customer.drunk_level)
 
     def test_sell_drink_to_customer_drink_found_no_stock(self):
         customer = Customer("Bob", 18, 25)
-        self.assertEqual("no stock", self.pub.sell_drink(customer, "Martini"))
+        self.assertEqual("no stock", self.pub.sell_item(customer, "drinks", "Martini"))
     
     def test_stock_value(self):
         self.assertEqual(700, self.pub.check_stock_total_value())
@@ -73,3 +73,8 @@ class TestPub(unittest.TestCase):
     def test_decrease_stock(self):
         self.pub.change_stock_amount("food", "Pizza", -3)
         self.assertEqual(27, self.pub.stock["food"][1].stock)
+
+    def test_sell_item_at_quantity(self):
+        customer = Customer("Zac", 100.00, 25)
+        self.pub.sell_item(customer, "drinks", "Bramble", 10)
+        self.assertEqual(1550, self.pub.till)
